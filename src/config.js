078b2a2b301c3519794c8ee16b84cd6a8f108702
vsharma1379 <viveksharma1379@gmail.com>
2023-01-07 12:07:22 +0530

    feat: added stipe keys

--- src/config.js
+++ src/config.js
@@ -1,25 +1,23 @@
 const dev = {
-	baseURL: "http://localhost:3080/api/",
-	landingPageUrl: "http://localhost:3080",
-	stripe: {
-		free: "price_1MKISQKDl4BWQxUPCjluSykk",
-		entry: "price_1MKISQKDl4BWQxUPCjluSykk",
-		pro: "price_1MKIi0KDl4BWQxUPbknrrmEH"
-	},
+  baseURL: "http://localhost:3080/api/",
+  landingPageUrl: "http://localhost:3080",
+  stripe: {
+    free: "price_1MNW04SB17XWitVzEcMoqiP0",
+    entry: "price_1MNVzISB17XWitVzUeoQxSOF",
+    pro: "price_1MNVwuSB17XWitVzyKOBUCsk",
+  },
 };
-  
+
 const prod = {
-	baseURL: '/api/',
-	landingPageUrl: "https://app.openaitemplate.com",
-	stripe: {
-		free: "price_1MKISQKDl4BWQxUPCjluSykk",
-		entry: "price_1MKISQKDl4BWQxUPCjluSykk",
-		pro: "price_1MKISQKDl4BWQxUPCjluSykk"
-	},
+  baseURL: "/api/",
+  landingPageUrl: "https://app.openaitemplate.com",
+  stripe: {
+    free: "price_1MNW04SB17XWitVzEcMoqiP0",
+    entry: "price_1MNVzISB17XWitVzUeoQxSOF",
+    pro: "price_1MNVwuSB17XWitVzyKOBUCsk",
+  },
 };
-  
-const config = process.env.NODE_ENV === 'development'
-	? dev
-	: prod;
-  
+
+const config = process.env.NODE_ENV === "development" ? dev : prod;
+
 export default config;
