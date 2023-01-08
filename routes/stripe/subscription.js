976cc37fa08e805b9cc465413c4ddae0ac6f303a
vsharma1379 <viveksharma1379@gmail.com>
2023-01-08 13:49:41 +0530

    log data

--- routes/stripe/subscription.js
+++ routes/stripe/subscription.js
@@ -1,95 +1,89 @@
-const models = require("../models")
+const models = require("../models");
 const User = models.user;
 
-const subscription = async (eventType,data) => {
+const subscription = async (eventType, data) => {
+  if (!eventType.includes("subscription")) {
+    return; // not a subscription event
+  }
 
-	if (!eventType.includes("subscription")) {
-		return // not a subscription event
-	}
+  console.log("subscription event detected", eventType);
 
-	console.log("subscription event detected",eventType)
-	
-	created(eventType,data)
-	updated(eventType,data)
-	deleted(eventType,data)
+  created(eventType, data);
+  updated(eventType, data);
+  deleted(eventType, data);
+};
 
-}
+const created = async (eventType, data) => {
+  if (!eventType.includes("subscription.created")) {
+    return; // not a subscription event
+  }
+  const { object } = data;
+  console.log("data", data);
+  console.log(`object.status`, object.plan.status);
+  console.log(`object.id`, object.id);
+  console.log(`object.customer`, object.customer);
+  console.log(`object.trial_end`, object.trial_end);
+  console.log(`object.current_period_end`, object.current_period_end);
+  console.log(`object.cancel_at_period_end`, object.cancel_at_period_end);
 
-const created = async (eventType,data) => {
+  await User.updateOne(
+    { customerId: object.customer },
+    {
+      status: object.status,
+      plan: object.items.data[0].plan.nickname,
+      trial_end: object.trial_end,
+      current_period_end: object.current_period_end,
+      cancel_at_period_end: object.cancel_at_period_end,
+    }
+  ).exec();
+};
 
-	if (!eventType.includes("subscription.created")) {
-		return // not a subscription event
-	}
-	const { object } = data
-	console.log(`object.status`,object.plan.status)
-	console.log(`object.id`,object.id)
-	console.log(`object.customer`,object.customer)
-	console.log(`object.trial_end`,object.trial_end)
-	console.log(`object.current_period_end`,object.current_period_end)
-	console.log(`object.cancel_at_period_end`,object.cancel_at_period_end)
+const updated = async (eventType, data) => {
+  if (!eventType.includes("subscription.updated")) {
+    return; // not a subscription event
+  }
+  const { object } = data;
+  console.log(`object.status`, object.plan.status);
+  console.log(`object.id`, object.id);
+  console.log(`object.customer`, object.customer);
+  console.log(`object.trial_end`, object.trial_end);
+  console.log(`object.current_period_end`, object.current_period_end);
+  console.log(`object.cancel_at_period_end`, object.cancel_at_period_end);
 
-	await User
-		.updateOne({ customerId: object.customer },
-			{ 
-				status: object.status,
-				plan: object.items.data[0].plan.nickname,
-				trial_end: object.trial_end,
-				current_period_end: object.current_period_end,
-				cancel_at_period_end: object.cancel_at_period_end,
-			})
-		.exec()
-}
+  await User.updateOne(
+    { customerId: object.customer },
+    {
+      status: object.status,
+      plan: object.items.data[0].plan.nickname,
+      trial_end: object.trial_end,
+      current_period_end: object.current_period_end,
+      cancel_at_period_end: object.cancel_at_period_end,
+    }
+  ).exec();
+};
 
-const updated = async (eventType,data) => {
+const deleted = async (eventType, data) => {
+  if (!eventType.includes("subscription.deleted")) {
+    return; // not a subscription event
+  }
+  const { object } = data;
+  console.log(`object.status`, object.plan.status);
+  console.log(`object.id`, object.id);
+  console.log(`object.customer`, object.customer);
+  console.log(`object.trial_end`, object.trial_end);
+  console.log(`object.current_period_end`, object.current_period_end);
+  console.log(`object.cancel_at_period_end`, object.cancel_at_period_end);
 
-	if (!eventType.includes("subscription.updated")) {
-		return // not a subscription event
-	}
-	const { object } = data
-	console.log(`object.status`,object.plan.status)
-	console.log(`object.id`,object.id)
-	console.log(`object.customer`,object.customer)
-	console.log(`object.trial_end`,object.trial_end)
-	console.log(`object.current_period_end`,object.current_period_end)
-	console.log(`object.cancel_at_period_end`,object.cancel_at_period_end)
-	
+  await User.updateOne(
+    { customerId: object.customer },
+    {
+      status: object.status,
+      plan: "N/A",
+      trial_end: object.trial_end,
+      current_period_end: object.current_period_end,
+      cancel_at_period_end: object.cancel_at_period_end,
+    }
+  ).exec();
+};
 
-	await User
-		.updateOne({ customerId: object.customer },
-			{ 
-				status: object.status,
-				plan: object.items.data[0].plan.nickname,
-				trial_end: object.trial_end,
-				current_period_end: object.current_period_end,
-				cancel_at_period_end: object.cancel_at_period_end,
-			})
-		.exec()
-}
-
-const deleted = async (eventType,data) => {
-
-	if (!eventType.includes("subscription.deleted")) {
-		return // not a subscription event
-	}
-	const { object } = data
-	console.log(`object.status`,object.plan.status)
-	console.log(`object.id`,object.id)
-	console.log(`object.customer`,object.customer)
-	console.log(`object.trial_end`,object.trial_end)
-	console.log(`object.current_period_end`,object.current_period_end)
-	console.log(`object.cancel_at_period_end`,object.cancel_at_period_end)
-	
-	await User
-		.updateOne({ customerId: object.customer },
-			{ 
-				status: object.status,
-				plan: "N/A",
-				trial_end: object.trial_end,
-				current_period_end: object.current_period_end,
-				cancel_at_period_end: object.cancel_at_period_end,
-		})
-	.exec()
-}
-
-
-module.exports = subscription
+module.exports = subscription;
